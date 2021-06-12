b4aff5646b9d337e837c0cdf784c534a945a45b1
Yuanlin Lin <im.yuanlinlin@gmail.com>
2021-06-12 11:04:48 +0800

    Added server status logs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "MiniSQLServer/MiniSQLServer.h"
+#include <iostream>
+
+using namespace std;
 
 int main(void) {
     MiniSQLServer server;
+    cout << "Starting server..." << endl;
+    cout << "Send POST request to port 3306 to execute SQL command." << endl;
     server.run();
+    cout << "Closing server ..." << endl;
 }
